5150f6a027ef09802860007d09f6902f3ca4181f
Wanderboy007 <ctejas44@gmail.com>
2022-10-13 19:54:10 +0530

    frontend added

--- js/index.js
+++ js/index.js
@@ -1,7 +1,26 @@
-
+const yt = document.getElementById("dropdown-cont");
+const btn = document.createElement("button");
+btn.classList.add("dropdowncontshow");
+document.querySelector().appendChild(".dropdowncontshow");
 
 
 function show_sub() {
+
     console.log("gerrkjsdb")
     document.getElementById("dropdown-cont").classList.toggle("dropdowncontshow");
+    let join = document.getElementById("m")
+    let t = document.createElement("div");
+    t.innerHTML = "tejas is a good boy";
+    join.appendchild(t)
+
+
+
 }
+// ++++++++++++++++++++++++++++++module maker+++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+
+
+
